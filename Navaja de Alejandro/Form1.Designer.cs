4b7f492c5d47fef38e567e4881d98a49114f46e8
Alejandro lopez <lopez18237@iesmarenostrum.com>
2017-03-09 19:36:03 +0100

    Primer commit y  push del proyecto

--- Navaja de Alejandro/Form1.Designer.cs
+++ Navaja de Alejandro/Form1.Designer.cs
@@ -36,9 +36,9 @@
             // 
             // Bapp1
             // 
-            this.Bapp1.Location = new System.Drawing.Point(115, 74);
+            this.Bapp1.Location = new System.Drawing.Point(104, 74);
             this.Bapp1.Name = "Bapp1";
-            this.Bapp1.Size = new System.Drawing.Size(209, 79);
+            this.Bapp1.Size = new System.Drawing.Size(220, 79);
             this.Bapp1.TabIndex = 0;
             this.Bapp1.Text = "button1";
             this.Bapp1.UseVisualStyleBackColor = true;
@@ -48,7 +48,7 @@
             // 
             this.Bapp2.Location = new System.Drawing.Point(509, 74);
             this.Bapp2.Name = "Bapp2";
-            this.Bapp2.Size = new System.Drawing.Size(206, 79);
+            this.Bapp2.Size = new System.Drawing.Size(217, 79);
             this.Bapp2.TabIndex = 1;
             this.Bapp2.Text = "button2";
             this.Bapp2.UseVisualStyleBackColor = true;
@@ -58,7 +58,7 @@
             // 
             this.Bapp4.Location = new System.Drawing.Point(509, 257);
             this.Bapp4.Name = "Bapp4";
-            this.Bapp4.Size = new System.Drawing.Size(206, 85);
+            this.Bapp4.Size = new System.Drawing.Size(217, 85);
             this.Bapp4.TabIndex = 2;
             this.Bapp4.Text = "button3";
             this.Bapp4.UseVisualStyleBackColor = true;
@@ -66,9 +66,9 @@
             // 
             // Bapp3
             // 
-            this.Bapp3.Location = new System.Drawing.Point(115, 257);
+            this.Bapp3.Location = new System.Drawing.Point(104, 257);
             this.Bapp3.Name = "Bapp3";
-            this.Bapp3.Size = new System.Drawing.Size(209, 85);
+            this.Bapp3.Size = new System.Drawing.Size(220, 85);
             this.Bapp3.TabIndex = 2;
             this.Bapp3.Text = "button3";
             this.Bapp3.UseVisualStyleBackColor = true;
